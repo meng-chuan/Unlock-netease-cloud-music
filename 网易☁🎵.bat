1a46fc4e6caef83cec864523640f6336fbf7ec78
DVLZY <384094169@qq.com>
2021-04-24 01:13:59 +0800

    Update 网易☁🎵.bat
    1、一键运行解锁服务和启动网易云音乐客户端
    2、后台运行服务

--- 网易☁🎵.bat
+++ 网易☁🎵.bat
@@ -1,3 +1,11 @@
-@echo off
+:: 启动网易云音乐（仅支持默认路径）
+start  "ProgramFiles%\Netease\CloudMusic\cloudmusic.exe" cloudmusic.exe
+
+:: 后台运行脚本
+@echo off 
+if "%1" == "h" goto begin 
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
+:begin 
+
 cd
 node app.js -p 52000 -f 59.111.181.38
